85a822cb38b70cac85945c3dcf94271cd13f3174
Anders Andreassen <andersjohanandreassen@gmail.com>
2019-03-30 00:13:47 -0700

    Updated fastjet files to new format

--- fastjet/jets_to_JUNIPR.cc
+++ fastjet/jets_to_JUNIPR.cc
@@ -1,5 +1,5 @@
 // compile:
-// g++ jets_to_JUNIPR.cc -o jets_to_JUNIPR `../../../Programs/fastjet-install/bin/fastjet-config --cxxflags --libs --plugins`
+// g++ jets_to_JUNIPR.cc -o jets_to_JUNIPR `../../../../programs/fastjet/fastjet-install/bin/fastjet-config --cxxflags --libs --plugins`
 // run:
 // ./jets_to_JUNIPR input_directory input_file output_directory recluster_def
 
@@ -7,7 +7,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include "fastjet_JUNIPR_utilities.cc"
+#include "CS_to_JuniprJet.cc"
 
 using namespace fastjet;
 using namespace std;
@@ -27,6 +27,9 @@ int main(int argc, char *argv[]) {
   string output_directory = argv[3];
   double recluster_def = atof(argv[4]);
 
+  string input_file_basename = input_file;
+  input_file_basename.erase(input_file_basename.find_last_of("."), string::npos);
+  
   // Input, output
   stringstream s_infile;
   s_infile << input_directory << "/" << input_file;
@@ -34,10 +37,10 @@ int main(int argc, char *argv[]) {
   infile.open( s_infile.str().c_str() );
 
   stringstream s_outfile;
-  s_outfile << output_directory << "/JUNIPR_format_" << input_file ;
+  s_outfile << output_directory << "/JuniprJets_" << input_file_basename << ".json" ;
   ofstream outfile;
   outfile.open( s_outfile.str().c_str());
-  
+  outfile << "{ \"JuniprJets\": [\n";
 
   // Read jets from input file
   // Jets begin with J <jet number> N <number of subjets>
@@ -49,13 +52,14 @@ int main(int argc, char *argv[]) {
   // Loop over jets
   int jet_counter = 0;
   while ((infile >> dummy) && (infile >> ijet) && (ijet <= NEVENTS)) {
-
     if (jet_counter % 10000 == 0) cout << "processing jet " << jet_counter << endl;
-
+    if (jet_counter>0){
+        outfile << ",\n";
+    }
     infile >> dummy >> nsubjets;
 
     vector<PseudoJet> particles;
-
+      
     // Particle loop
     for (int i = 0; i < nsubjets; i++) {
 
@@ -64,11 +68,10 @@ int main(int argc, char *argv[]) {
 
       PseudoJet particle(px, py, pz, e);
       particles.push_back(particle);
-
     }  // end loop (particles)
     
     // Recluster jet constituents
-    JetDefinition reclust_def(ee_genkt_algorithm, 7, recluster_def); // radius unused
+    JetDefinition reclust_def(genkt_algorithm, 7, recluster_def); // radius unused
     ClusterSequence reclust_seq(particles, reclust_def);
     vector<PseudoJet> reclust_jets = reclust_seq.exclusive_jets(1);
     if (jet_counter == 0)
@@ -76,12 +79,14 @@ int main(int argc, char *argv[]) {
     if(reclust_jets[0].constituents().size() != nsubjets){
       cout << "Mismatch between nsubjets and reclustered jet" << endl;
     }
-    outfile << "J " << jet_counter << endl;
-    convert_cluster_sequence_to_JUNIPR(reclust_seq, outfile);
-  
+    
+      
+    JuniprJet jet = cluster_sequence_to_JuniprJet(reclust_seq);
+    jet.write_to_json(outfile);
+    
     jet_counter++;
 } // end loop (jets)
-
+    outfile << "\n]\n}";
 
 
   infile.close();
